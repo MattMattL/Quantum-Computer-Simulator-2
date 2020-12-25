a35f91a45d7503eedef6a3d2e1450ae7a8c0361c
Matt L <primeMuon@gmail.com>
2020-12-25 02:44:26 +0000

    Add initial Qubits.hpp file

---
+++ qubits.hpp
@@ -0,0 +1,18 @@
+#ifndef QUMULATOR_QUBITS_HPP
+#define QUMULATOR_QUBITS_HPP
+
+#include "complex.hpp"
+#include "matrix.hpp"
+
+#define Qubit Complex
+
+template<class T>
+class Qubits
+{
+private:
+
+public:
+	
+};
+
+#endif
