15ba0f84158734a33df2021ee1538933f33e0aab
Matt L <primeMuon@gmail.com>
2020-12-26 06:36:32 +0000

    Fix initialisation code

--- qubits.hpp
+++ qubits.hpp
@@ -3,20 +3,25 @@
 
 #include "complex.hpp"
 #include "matrix.hpp"
+#include "quantum_gates.hpp"
 
 template<class T>
 class Qubits
 {
 private:
 	Complex<T> *states;
-
 	unsigned int numQubits;
 	unsigned int numOfCoeffs;
 
+	QuantumGates<double> gate;
+
 public:
+	/* Constructor and Deconstructor */
 	Qubits(int);
 	~Qubits();
 };
+
+/* Constructor and Deconstructor */
 
 template<class T>
 Qubits<T>::Qubits(int qubits)
@@ -25,6 +30,9 @@ Qubits<T>::Qubits(int qubits)
 	numOfCoeffs = 1 << numQubits;
 
 	states = new Complex<T>[numOfCoeffs];
+
+	for(int i=0; i<numOfCoeffs; i++)
+		states[i].set(1, 0);
 }
 
 template<class T>
