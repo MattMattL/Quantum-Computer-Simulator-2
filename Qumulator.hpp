a536620cc41e43e2fdc399e10842e03732723eec
Matt L <primeMuon@gmail.com>
2021-01-07 20:53:10 +0000

    Add header file Qumulator.hpp

---
+++ Qumulator.hpp
@@ -0,0 +1,19 @@
+/*
+	Include this Qumulator.hpp as a header file.
+*/
+
+#ifndef QUMULATOR_QUMULATOR_HPP
+#define QUMULATOR_QUMULATOR_HPP
+
+// The main library with Qubits<>::
+#include "qubits.hpp"
+
+// Qumulator visualisation library
+#include "qumulator_graphics.hpp"
+
+// Other libraries used
+#include "complex.hpp"
+#include "matrix.hpp"
+#include "quantum_gates.hpp"
+
+#endif
