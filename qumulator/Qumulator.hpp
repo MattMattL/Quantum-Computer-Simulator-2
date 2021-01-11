a49718c488bce26756697e27aa17c5c8fdaeb393
Matt L <primeMuon@gmail.com>
2021-01-11 01:01:25 +0000

    Fix comments

--- qumulator/Qumulator.hpp
+++ qumulator/Qumulator.hpp
@@ -5,15 +5,10 @@
 #ifndef QUMULATOR_QUMULATOR_HPP
 #define QUMULATOR_QUMULATOR_HPP
 
-// The main library with Qubits<>::
-#include "qubits.hpp"
-
-// Qumulator visualisation library
-#include "qumulator_graphics.hpp"
-
-// Other libraries contained
 #include "complex.hpp"
 #include "matrix.hpp"
 #include "quantum_gates.hpp"
+#include "qubits.hpp"
+#include "qumulator_graphics.hpp"
 
 #endif
